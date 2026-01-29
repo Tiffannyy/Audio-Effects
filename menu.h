b73a76ce8c2a01e31a179f4d319c94e3fdcc1613
tiffany liu <tiffanxl@uci.edu>
2026-01-29 11:49:02 -0800

    Co-authored-by: Nathaniel Kalaw <kalaw-n@users.noreply.github.com>

--- menu.h
+++ menu.h
@@ -10,12 +10,46 @@
 */
 
 #include <iostream>
-#include <stdlib.h>
 
 #include "types.h"
 
+// Choice function
+void choiceSelect(std::string choice, EffectChoices &effectChoice, bool &validChoice){
+    switch (choice){
+        case '0':
+            std::cout << "Ending program..." << std::endl;
+            return false; // request graceful exit to caller
+            break;
+        case '1':                       // no effect
+            effectChoice.norm = true;
+            validChoice = true;
+            break;
+        case '2':                       // tremolo
+            effectChoice.trem = true;
+            validChoice = true;
+            break;
+        case '3':
+            effectChoice.delay = true;  // delay
+            validChoice = true;
+            break;
+        case '4':
+            effectChoice.reverb = true; // reverb
+            validChoice = true;
+            break;
+        case '5':
+            effectChoice.bitcrush = true; // bitcrush
+            validChoice = true;
+            break;
+        case '6':
+            effectChoice.dirt = true;
+            validChoice = true;
+        default:
+            break;
+    }
+}
+
 // Menu function
-bool menuFunction(EffectChoices& effectChoice){
+bool menuFunction(EffectChoices &effectChoice){
     bool validChoice = false;
     char userChoice;
     std::cout << "*------ Audio Effects Program ------*\n" << "Effects Options:\n";
@@ -25,71 +59,17 @@ bool menuFunction(EffectChoices& effectChoice){
     std::cout << "(3) Delay" << std::endl;    
     std::cout << "(4) Reverb" << std::endl;            
     std::cout << "(5) Bitcrush" << std::endl;
-//    std::cout << "(4) Volume Adjuster" << std::endl;
+    std::cout << "(6) Dirt" << std::endl;
+
     std::cout << "Enter the integer value of the effect you would like to apply: ";
     std::cin >> userChoice;
-    
-    switch (userChoice){
-            case '0':
-                std::cout << "Ending program..." << std::endl;
-                return false; // request graceful exit to caller
-                break;
-            case '1':                       // no effect
-                effectChoice.norm = true;
-                validChoice = true;
-                break;
-            case '2':                       // tremolo
-                effectChoice.trem = true;
-                validChoice = true;
-                break;
-            case '3':
-                effectChoice.delay = true;  // delay
-                validChoice = true;
-                break;
-            case '4':
-                effectChoice.reverb = true; // reverb
-                validChoice = true;
-                break;
-            case '5':
-                effectChoice.bitcrush = true;
-                validChoice = true;
-                break;
-            default:
-                break;
-        }
-    
+
+    choiceSelect(userChoice, effectChoice, validChoice);
+
     while (!validChoice){
-    std::cout << "Enter a valid option: ";
-    std::cin >> userChoice; 
-        switch (userChoice){
-            case '0':
-                std::cout << "Ending program..." << std::endl;
-                return false; // request graceful exit to caller
-                break;
-            case '1':                       // no effect
-                effectChoice.norm = true;
-                validChoice = true;
-                break;
-            case '2':                       // tremolo
-                effectChoice.trem = true;
-                validChoice = true;
-                break;
-            case '3':
-                effectChoice.delay = true;  // delay
-                validChoice = true;
-                break;
-            case '4':
-                effectChoice.reverb = true; // reverb
-                validChoice = true;
-                break;
-            case '5':
-                effectChoice.bitcrush = true; // bitcrush
-                validChoice = true;
-                break;
-            default:
-                break;
-        }
-      
+        std::cout << "Enter a valid option: ";
+        std::cin >> userChoice; 
+        choiceSelect(userChoice, effectChoice, validChoice);
     }
     return true;
 }
